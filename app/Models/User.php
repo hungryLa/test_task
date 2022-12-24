630cf343467b0e894aed387b0ab142e5ee8a45b0
hungryLa <hungry_la@mail.ru>
2022-12-24 21:30:45 +0500

    Реализовал модели [User,News,Comment], связи между ними и миграции

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
+
+    public function news(){
+        return $this->hasMany(News::class);
+    }
 }
