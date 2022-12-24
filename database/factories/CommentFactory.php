4961e0fcbf5bad20a8d962a6b4ad2dfc2fd6c976
hungryLa <hungry_la@mail.ru>
2022-12-24 21:55:04 +0500

    Создал фабрики для моделей и заполнил БД

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\News;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'content' => $this->faker->text,
+            'likes' => random_int(1, 300),
+            'user_id' => User::get()->random()->id,
+            'news_id' => News::get()->random()->id,
+        ];
+    }
+}
---
+++ database/factories/NewsFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class NewsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title'=> $this->faker->sentence(2),
+            'description' => $this->faker->text(),
+            'small_description' => $this->faker->sentence(6),
+            'user_id' => User::get()->random()->id,
+        ];
+    }
+}
